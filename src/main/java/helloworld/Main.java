1e18ed6365f420b3d8302bb6ff7785a90f366d71
noonlit <julietfeynman@gmail.com>
2020-10-03 16:18:33 +0300

    Add base files

---
+++ src/main/java/helloworld/Main.java
@@ -0,0 +1,13 @@
+package helloworld;
+
+public class Main {
+    public static void main(String[] args) {
+        int Teodora = 2;
+        int Andra = 0;              //please choose a number
+        int numberOfRepetitions = Teodora + Andra;
+        while (numberOfRepetitions > 0){
+            System.out.println("Hello, World "+numberOfRepetitions+"!");
+            numberOfRepetitions--;
+        }
+    }
+}
